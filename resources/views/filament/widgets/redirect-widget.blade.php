cc4d487660d70d5d660aa4621b2178efa32c8230
Iqbal Firmansyah <fqbal3@gmail.com>
2024-12-17 19:57:26 +0700

    change redirect button

--- resources/views/filament/widgets/redirect-widget.blade.php
+++ resources/views/filament/widgets/redirect-widget.blade.php
@@ -1,11 +1,15 @@
 <x-filament-widgets::widget>
     <x-filament::section>
-        <div class="flex flex-col items-center justify-center text-gray-500 dark:text-gray-400">
+        <div class="flex flex-col items-center justify-center text-gray-500 p-2">
             <h1 class="text-lg font-semibold">Daftar Display</h1>
-            <p class="text-sm">
-                Buka Display : <a href="{{ route('display') }}"
-                    class="underline text-primary-600" target="_blank" onclick="window.open('{{ route('display') }}', 'newwindow', 'width=1366, height=768'); return false;">Display 1</a>.
-            </p>
+
+            <div class="flex mb-4 items-start justify-end w-full">
+                <button type="button"
+                    class="m-2 py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 text-dark hover:border-blue-600 hover:text-blue-600 focus:outline-none focus:border-blue-600 focus:text-blue-600 disabled:opacity-50 disabled:pointer-events-none" onclick="window.open('{{ route('display') }}', 'newwindow', 'width=1366, height=768'); return false;">
+                    Buka Display 1
+                </button>
+            </div>
+
 
             <div style="position: relative; width: 100%; height: 100vh;">
                 <iframe src="{{ route('display') }}" frameborder="0" scrolling="yes" style="width: 100%; height: 100%;">
