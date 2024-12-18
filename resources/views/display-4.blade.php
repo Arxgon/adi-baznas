514322843eb80ea8158ac77959d06b953686e0f2
Iqbal Firmansyah <fqbal3@gmail.com>
2024-12-18 16:23:52 +0700

    add pre build resource for build test

--- resources/views/display-4.blade.php
+++ resources/views/display-4.blade.php
@@ -138,54 +138,96 @@
                         </div>
                     </div>
                     {{-- Social Media Section --}}
-                    <div class="p-3 flex h-full flex-col bg-white border shadow-sm rounded-xl">
+                    <div class="p-2 flex h-full flex-col bg-white border shadow-sm rounded-xl justify-center items-center">
                         <!-- List -->
-                        <div class="space-y-3">
+                        <div class="space-y-1">
                             <dl class="flex flex-col sm:flex-row gap-1">
-                                <dt class="min-w-40">
-                                    <span class="block text-sm text-gray-500 dark:text-neutral-500">Website</span>
+                                <dt class="min-w-32 flex flex-row items-center">
+                                    <span class="inline-flex justify-center items-center w-[40px] rounded-full text-green-800 dark:text-blue-500">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                            stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-phone">
+                                            <path
+                                                d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z" />
+                                        </svg>
+                                    </span>
+                                    <span class="block text-sm text-gray-500 dark:text-neutral-500">Telp.</span>
                                 </dt>
-                                <dd>
+                                <dd class="flex flex-row items-center">
                                     <ul>
                                         <li
                                             class="me-1inline-flex items-center text-sm text-gray-800 dark:text-neutral-200">
                                             :
-                                            jateng.baznas.go.id
+                                            024-76444206
                                         </li>
                                     </ul>
                                 </dd>
                             </dl>
-
                             <dl class="flex flex-col sm:flex-row gap-1">
-                                <dt class="min-w-40">
-                                    <span class="block text-sm text-gray-500 dark:text-neutral-500">Kontak</span>
+                                <dt class="min-w-32 flex flex-row items-center">
+                                    <span class="inline-flex justify-center items-center w-[40px] rounded-full text-green-800 dark:text-blue-500">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                            stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-printer-check">
+                                            <path d="M13.5 22H7a1 1 0 0 1-1-1v-6a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1v.5" />
+                                            <path d="m16 19 2 2 4-4" />
+                                            <path d="M6 18H4a2 2 0 0 1-2-2v-5a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v2" />
+                                            <path d="M6 9V3a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1v6" />
+                                        </svg>
+                                    </span>
+                                    <span class="block text-sm text-gray-500 dark:text-neutral-500">Fax</span>
                                 </dt>
-                                <dd>
+                                <dd class="flex flex-row items-center">
                                     <ul>
                                         <li
-                                            class="me-1 inline-flex items-center text-sm text-gray-800 dark:text-neutral-200">
+                                            class="me-1inline-flex items-center text-sm text-gray-800 dark:text-neutral-200">
                                             :
-                                            0822-5000-4491
+                                            024-76440191
                                         </li>
-
                                     </ul>
                                 </dd>
                             </dl>
-
                             <dl class="flex flex-col sm:flex-row gap-1">
-                                <dt class="min-w-40">
-                                    <span class="block text-sm text-gray-500 dark:text-neutral-500">Instagram</span>
+                                <dt class="min-w-32 flex flex-row items-center">
+                                    <span class="inline-flex justify-center items-center w-[40px] rounded-full text-green-800 dark:text-blue-500">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                            stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-mail">
+                                            <rect width="20" height="16" x="2" y="4" rx="2" />
+                                            <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
+                                        </svg>
+                                    </span>
+                                    <span class="block text-sm text-gray-500 dark:text-neutral-500">E-mail</span>
                                 </dt>
-                                <dd>
+                                <dd class="flex flex-row items-center">
                                     <ul>
                                         <li
-                                            class="me-1 inline-flex items-center text-sm text-gray-800 dark:text-neutral-200">
+                                            class="me-1inline-flex items-center text-sm text-gray-800 dark:text-neutral-200">
                                             :
-                                            baznasjateng
+                                            baznasprov.jateng@baznas.go.id
                                         </li>
                                     </ul>
                                 </dd>
                             </dl>
+                            <dl class="flex flex-col sm:flex-row gap-1">
+                                <dt class="min-w-32 flex flex-row items-center">
+                                    <span class="inline-flex justify-center items-center w-[40px] rounded-full text-green-800 dark:text-blue-500">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-whatsapp"
+                                            viewBox="0 0 16 16">
+                                            <path
+                                                d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232" />
+                                        </svg>
+                                    </span>
+                                    <span class="block text-sm text-gray-500 dark:text-neutral-500">Whats App</span>
+                                </dt>
+                                <dd class="flex flex-row items-center">
+                                    <ul>
+                                        <li
+                                            class="me-1inline-flex items-center text-sm text-gray-800 dark:text-neutral-200">
+                                            :
+                                            082250004491
+                                        </li>
+                                    </ul>
+                                </dd>
+                            </dl>
+
 
                         </div>
                         <!-- End List -->
