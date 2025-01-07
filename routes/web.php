82d7f91381a57fcc00eed6493b14b4369a452ef7
Iqbal Firmansyah <fqbal3@gmail.com>
2025-01-07 13:34:49 +0700

    minor bug fix

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('display-4');
+});
 
 Route::get('/display', function () {
     return view('display-4');
 })->name('display');
-Route::get('/display-2', function () {
-    return view('display-2');
-});
-Route::get('/display-3', function () {
-    return view('display-3');
-});
+// Route::get('/display-2', function () {
+//     return view('display-2');
+// });
+// Route::get('/display-3', function () {
+//     return view('display-3');
+// });
